4b9ffc2bb05c01b8c1436c3ac10f91b0c15fecdf
33 <yuluolfs@163.com>
2022-12-30 22:47:13 +0800

    Fix severe memory leak when reading 2D-SAFEARRAY

--- src/library/tLuaCOMTypeHandler.cpp
+++ src/library/tLuaCOMTypeHandler.cpp
@@ -1362,7 +1362,7 @@ stkIndex tLuaCOMTypeHandler::get_from_array(lua_State* L,
   HRESULT hr = SafeArrayGetElement(safearray, indices, pv);
 
   if(FAILED(hr)) {
-    if (vt != VT_VARIANT) {
+    if (vt == VT_VARIANT) {
       VariantClear(&varg);
     }
     LUACOM_EXCEPTION(INTERNAL_ERROR);
@@ -1370,7 +1370,7 @@ stkIndex tLuaCOMTypeHandler::get_from_array(lua_State* L,
 
   com2lua(L, varg);
 
-  if (vt != VT_VARIANT) {
+  if (vt == VT_VARIANT) {
     VariantClear(&varg);
   }
 
